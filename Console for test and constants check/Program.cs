40e4242ea2c2b0deff48c259c7075469373d04d7
Pantorius <flyspore@yandex.ru>
2025-03-21 18:12:04 +0300

    Public code

---
+++ Console for test and constants check/Program.cs
@@ -0,0 +1,188 @@
+﻿using System.Diagnostics;
+using HashSetLibrary;
+namespace Console_for_test_and_constants_check
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double mike = 0;
+            double ivanAlt = 0;
+
+            for (int p = 0; p < 10; p++)
+            {
+                
+                HashSet<string> randDic = new HashSet<string>();
+                Dictionary<string, int> MicrosoftDic = new Dictionary<string, int>();
+                IvanHashTableAlternative<string, int> ivanDic = new IvanHashTableAlternative<string, int>();
+
+                Random rnd = new Random();
+                for (int i = 0; i < 1e6; i++)
+                {
+                    randDic.Add((i + rnd.Next()).ToString());
+                }
+                Stopwatch sw = new Stopwatch();
+
+                List<string> wordsForTest = randDic.ToList();
+
+                sw.Restart();
+                foreach (string s in wordsForTest)
+                {
+                    MicrosoftDic.Add(s, s.Length);
+                }
+                sw.Stop();
+                Console.WriteLine(sw.Elapsed);
+                mike += sw.Elapsed.TotalMilliseconds;
+
+                sw.Restart();
+                foreach (string s in wordsForTest)
+                {
+                    ivanDic.Add(s, s.Length);
+                }
+                sw.Stop();
+                Console.WriteLine(sw.Elapsed);
+                ivanAlt += sw.Elapsed.TotalMilliseconds;
+                
+                Console.WriteLine(MicrosoftDic.Count == ivanDic.Count);
+            }
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"Mike {mike / 10} ms");
+            Console.WriteLine($"ivanAlt {ivanAlt / 10} ms");
+            Console.ForegroundColor = ConsoleColor.Gray;
+
+            TakeWarAndPeaceElements();
+
+
+            //FindingNumbers();
+        }
+
+
+        static void TakeWarAndPeaceElements()
+        {
+            string[] words;
+            using (StreamReader sr = new StreamReader("War and peace.txt"))
+            {
+                words = sr.ReadToEnd().Split(new char[] {' ', ',','.' ,'!','?',':', ';', '-' }, StringSplitOptions.RemoveEmptyEntries);
+            }
+
+            Dictionary<string, int> microsoft = new Dictionary<string, int>();
+            IvanHashTableAlternative<string, int> ivanHash = new IvanHashTableAlternative<string, int>();
+            int outCount;
+            Stopwatch sw = new Stopwatch();
+
+            sw.Start();
+            foreach(string s in words)
+            {
+                if(microsoft.TryGetValue(s, out outCount))
+                {
+                    microsoft.Remove(s);
+                    microsoft.Add(s, outCount+1);
+                }
+                else
+                {
+                    microsoft.Add(s, 1);
+                }
+            }
+            sw.Stop();
+            Console.WriteLine($"{sw.Elapsed} Microsoft add War");
+
+
+            sw.Restart();
+            foreach (string s in words)
+            {
+                if (ivanHash.TryGetValue(s, out outCount))
+                {
+                    ivanHash.Remove(s);
+                    ivanHash.Add(s, outCount + 1);
+                }
+                else
+                {
+                    ivanHash.Add(s, 1);
+                }
+            }
+            sw.Stop();
+            Console.WriteLine($"{sw.Elapsed} Ivan add War");
+
+            List<KeyValuePair<string, int>> l = ivanHash.GetArray().ToList();
+    
+            sw.Restart();
+            foreach(KeyValuePair<string, int> pair in l)
+            {
+                if(pair.Value > 27)
+                {
+                    microsoft.Remove(pair.Key);
+                }
+            }
+            sw.Stop();
+            Console.WriteLine($"{sw.Elapsed} Microsoft rm words");
+
+            sw.Restart();
+            foreach (KeyValuePair<string, int> pair in l)
+            {
+                if (pair.Value > 27)
+                {
+                    ivanHash.Remove(pair.Key);
+                }
+            }
+            sw.Stop();
+            Console.WriteLine($"{sw.Elapsed} Ivan rm words");
+
+
+            Console.WriteLine(microsoft.Count == ivanHash.Count);
+
+            foreach(KeyValuePair<string, int> pair in microsoft)
+            {
+                if (!ivanHash.Contains(pair.Key))
+                {
+                    Console.WriteLine(false);
+                }
+            }
+        }
+
+        
+
+
+        static void FindingNumbers()
+        {
+            (int, int) t;
+            for (int i = 2; i < int.MaxValue / 2; i *= 2)
+            {
+                t = FindConstantsForNumber(i);
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("{0}, {1}, {2}", t.Item1, t.Item2, i);
+                Console.ForegroundColor = ConsoleColor.Gray;
+            }
+        }
+
+        static (int, int) FindConstantsForNumber(long two)
+        {
+            int c2 = 25578;
+            int c1 = 1361;
+            while (!BijectionCheck(c1, c2, two) && c1 < int.MaxValue / 1000)
+            {
+                c2 += 2;
+                if(c1 > two * 100000)
+                {
+                    c2 = 10;
+                    c1 += 2;
+                    Console.WriteLine($"{c1}");
+                }
+            }
+            return (c1, c2);
+        }
+
+        static bool BijectionCheck(long c1, long c2, long two)
+        {
+            HashSet <long> set = new HashSet<long>();
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            for(long i = 0; i < two; i++)
+            {
+                set.Add(((c1 * i) % two + c2 * (i * i) % two) % two);
+            }
+            sw.Stop();
+            Console.WriteLine("{0} , {1}, {2}, c1 {3}, c2 {4}", set.Count, two, sw.Elapsed, c1, c2);
+            return set.Count == two;
+        }
+    }
+}
---
+++ HashSetLibrary/IvanHashSet.cs
@@ -0,0 +1,219 @@
+﻿namespace HashSetLibrary
+{
+    class Node<TKey, TValue>
+    {
+        public TKey key = default(TKey);
+        public TValue value = default(TValue);
+        public bool exist = false;
+        public int hash = default(int);
+        public Node(ref TKey key, ref TValue value, int hash)
+        {
+            this.key = key;
+            this.value = value;
+            this.exist = true;
+            this.hash = hash;
+        }
+        public Node() { }
+    }
+
+
+    public class IvanHashTableAlternative<TKey, TValue> where TKey : IEquatable<TKey>
+    {
+        Node<TKey, TValue>[] nodes;
+        static int[] quadricConstants = new int[0];
+        int count;
+        int capacity;
+        //int maxIterations = 5;
+        int c1 = 3;
+        int c2 = 10;
+        int mod;
+
+
+        Node<TKey, TValue> newNode;
+        delegate bool Comparer<T>(object x, object y);
+        Comparer<TKey> comp = Comparer<TKey>.Equals; //Делегат для лучшего сравнения ключей. Подстраивается под тип TKey
+        double refillFactor = 0.75;
+        int index; //глобальные переменные, чтобы не выделять постоянно память
+        int hash;
+
+        public int Count { get { return count; } }
+        public int Capacity { get { return capacity; } }
+        public IvanHashTableAlternative(int capacity)
+        {
+            nodes = new Node<TKey, TValue>[capacity];
+            this.capacity = capacity;
+            mod = capacity - 1;
+            if (typeof(TKey) == typeof(string))
+            {
+                comp = string.Equals;
+            }
+            CountQuadricConst();
+        }
+        public IvanHashTableAlternative() : this(4) { }
+
+        void CountQuadricConst() //Считаем константы суммы для квадратичного исследования, чтобы не терять время в процессе.
+        {
+            quadricConstants = new int[int.MaxValue / 1024];
+            for(int i = 0; i <  quadricConstants.Length; i++)
+            {
+                quadricConstants[i] = c1 * i + c2 * i * i;
+            }
+        }
+
+        public void Add(TKey key, TValue value)
+        {
+            if(key is null) throw new ArgumentNullException("key is null");
+            hash = key.GetHashCode();
+            newNode = new Node<TKey, TValue>(ref key, ref value,hash);
+            //newNode = new Node<TKey, TValue>(ref key, ref value, hash);
+            if (!HashingItemInArray())
+            {
+                Resize();
+                if (!HashingItemInArray())
+                {
+                    throw new ArithmeticException("Can't insert this key into hash set");
+                }
+            }
+            if (count > capacity * refillFactor)
+            {
+                Resize();
+            }
+        }
+        bool HashingItemInArray()
+        {
+            int i = 0;
+            while (i < capacity)
+            {
+                index = (hash + quadricConstants[i]) & mod;//квадратичное исследование за счёт размера массива в степень двойки, побитовое И работает, как модуль
+                if (nodes[index] == null || !nodes[index].exist)
+                {
+                    nodes[index] = newNode; //new Node<TKey, TValue>(ref key, ref value, hash);
+                    ++count;
+                    break;
+                }
+                if (comp(nodes[index].key, newNode.key))
+                {
+                    throw new ArgumentException("This key is already exist");
+                }
+                ++i;
+            }
+            if (i >= capacity) return false; //случай, когда мы не смогли добавить элемент в массив
+            if (i > capacity * refillFactor) Resize();
+            return true;
+        }
+
+        public bool Contains(TKey key)
+        {
+            if (count == 0) throw new InvalidOperationException();
+            TValue tmp;
+            return FindOrRemAction(key, false, out tmp);
+        }
+
+        public bool Remove(TKey key)
+        {
+            if (count == 0) throw new InvalidOperationException();
+            TValue tmp;
+            return FindOrRemAction(key, true, out tmp);
+        }
+
+        public bool TryGetValue(TKey key, out TValue value)
+        {
+            if (count == 0)
+            {
+                value = default;
+                return false;
+            }
+            TValue tmp;
+            bool b = FindOrRemAction(key, false, out tmp);
+            value = tmp;
+            return b;
+        }
+
+        bool FindOrRemAction(TKey key, bool rm, out TValue value)
+        {
+            if (key == null) throw new ArgumentNullException("Key can't be null here");
+            int i = 0;
+            hash = key.GetHashCode(); ; //EqualityComparer.Default соответствующий TKey
+
+            while (i < capacity)
+            {
+                index = (hash + quadricConstants[i]) & mod; //квадратичное исследование
+                if (nodes[index] == null)
+                {
+                    value = default;
+                    return false;
+                }
+                if (nodes[index].exist && comp(nodes[index].key, key))
+                {
+                    value = nodes[index].value;
+                    if (rm)
+                    {
+                        nodes[index].exist = false;
+                        --count;
+                    }
+                    return true;
+                }
+                ++i;
+            }
+            value = default;
+            return false;
+        }
+
+
+        void Resize()
+        {
+            capacity = capacity << 1;
+            mod = capacity - 1;
+            Node<TKey, TValue>[] newNodes = new Node<TKey, TValue>[capacity];
+            for (int i = 0; i < nodes.Length; i++)
+            {
+                if (nodes[i] != null && nodes[i].exist)
+                    HashingInResize(i, ref newNodes);
+
+            }
+            nodes = newNodes;
+        }
+        void HashingInResize(int nodeIndex, ref Node<TKey,TValue>[] newNodes)
+        {
+            int i = 0;
+            //int mod = capacity - 1;
+            // & (capacity - 1);
+            while (true)
+            {
+                index = (nodes[nodeIndex].hash + quadricConstants[i]) & mod;
+                if (newNodes[index] == null)
+                {
+                    newNodes[index] = nodes[nodeIndex];
+                    return;
+                }
+                ++i;
+            }
+            throw new ArithmeticException("Can't resize hash table properly");
+        }
+
+
+        public IEnumerable<KeyValuePair<TKey, TValue>> GetArray()
+        {
+            List<KeyValuePair<TKey, TValue>> lst = new List<KeyValuePair<TKey, TValue>>();
+            for(int i = 0;i < nodes.Length; ++i)
+            {
+                if (nodes[i] != null && nodes[i].exist)
+                {
+                    lst.Add(new KeyValuePair<TKey, TValue>(nodes[i].key, nodes[i].value));
+                }
+            }
+            return lst;
+        }
+
+
+        //int ReturnHash(TKey key)
+        //{
+        //    return key.GetHashCode() & (capacity - 1);
+        //}
+
+        //int ReturnHash2(ref TKey key)
+        //{
+        //    return (key.GetHashCode() & 0x7FFFFFFF) % capacity;
+        //}
+    }
+}
